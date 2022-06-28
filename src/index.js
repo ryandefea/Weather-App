2b2b52babd40c8d5cf4315348291e6be8c1e761c
ryandefea <ryan.defea@gmail.com>
2022-06-27 18:29:58 -0700

    fix js
    Co-authored-by: Jacob Palaoro <jpalaoro197@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,4 +2,39 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Template from './js/template.js';
+
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    const city = $('#city').val();
+    const state = $('#state').val();
+    const lon = $('#longitude').val();
+    const lat = $('#latitude').val();
+    $('#city').val("");
+    $('#state').val("");
+    $('#latitude').val("");
+    $('#longitude').val("");
+    
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}lon= ${lon}&appid=${process.env.API_KEY}`;
+    
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+   function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city},${state} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${(Math.round(((response.main.temp)-273.15)*1.8)+32)} degrees.`);
+      $('.showSpeed').text(`The wind speed is ${response.wind.speed} mph.`)
+      $('.showSky').text(`with ${response.weather[0].description}`)
+    }
+  });
+});
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const Dotenv = require('dotenv-webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -12,11 +12,11 @@ module.exports = {
   devServer: {                 
     contentBase: './dist'      
   },
-  plugins: [
+  plugins: [new Dotenv(),
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
       template: './src/index.html',
-      inject: 'body'
+      inject: 'body',
     })
   ],
   module: {
